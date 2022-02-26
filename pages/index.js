a9cb956851b9810c299fa76d6d233c98af3f1e3a
Kekelberi <alexei.gaponov@gmail.com>
2022-02-26 16:47:25 +0300

    save

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import Headers from "../src/components/Header";
-
+import Headers from '../src/components/Header'
+import Head from 'next/head'
 export default function Home() {
- 
-  
   return (
     <div>
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+    
+      </Head>
       <Headers />
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,37 @@ const Headers = () => {
         <header className='header'>
           <nav>
             <ul className='nav'>
-              <li className='nav-item'>Для мужчин</li>
-              <li className='nav-item'>Для женщин</li>
-              <li className='nav-item'>Для детей</li>
-            </ul>
-            <img src='./logo.svg' alt='logo' />
-            <ul className='nav'>
-              <li className='nav-item'>Оплата</li>
-              <li className='nav-item'>Доставка</li>
+              <li className='nav-item'>
+                <a className='nav-link' href='!#'>
+                  Для мужчин
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='!#'>
+                  Для женщин
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='!#'>
+                  Для детей
+                </a>
+              </li>
             </ul>
           </nav>
+          <img src='./logo.svg' alt='logo' />
+          <ul className='nav'>
+            <li className='nav-item'>
+              <a className='nav-link' href='!#'>
+                Оплата
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='!#'>
+                Доставка
+              </a>
+            </li>
+          </ul>
+          <div>5</div>
         </header>
       </div>
     </div>
